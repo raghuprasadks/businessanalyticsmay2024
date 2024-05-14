f7ef2de2f754c97df1be3373a9e14ac4c46ef894
raghuprasadks <prasadraghuks@gmail.com>
2024-05-14 10:55:24 +0530

    updated

---
+++ programs/12-loops.py
@@ -0,0 +1,61 @@
+"""
+Loops - while
+"""
+"""
+print(" Display 1 to 10 using while loop")
+start = 1
+end = 10
+while (start <= end):
+    print(start)
+    start = start + 1
+"""
+"""
+print(" 5' table .. 5,10,15 to 50")
+start = 5
+end = 50
+while (start <= end):
+    print(start)
+    start = start + 5
+"""
+"""
+print(" 5' table in reverse .. 50,45,...5")
+start = 50
+end = 5
+while (start >= end):
+    print(start)
+    start = start - 5
+"""
+"""
+print("for loop")
+print("1's table using for loop - 0 to 9")
+# range(10) .. 0,1,2,9
+for i in range(10):
+    print(i)
+print("1's table using for loop - 1 to 10")
+# range(1,11) .. 1,2,3,10
+for i in range(1,11):
+    print(i)
+"""
+"""
+print("5's table using for loop - 5 to 50")
+# range(5,51,5) .. 5,10,15...50
+for i in range(5,51,5):
+    print(i)
+"""
+print("5's table using for loop in reverse order- 50 to 5")
+for i in range(50,4,-5):
+    print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
